5662fb0a97b8f609653930f92e521d8aab51f3e9
adithyakini <67313225+adithyakini@users.noreply.github.com>
2026-05-24 10:42:17 +0530

    Update genres.py

--- theory/genres.py
+++ theory/genres.py
@@ -1,1 +1,29 @@
+GENRE_PROGRESSIONS = {
 
+    "Pop": [
+        "C",
+        "G",
+        "Am",
+        "F"
+    ],
+
+    "Rock": [
+        "C",
+        "G",
+        "Am",
+        "F"
+    ],
+
+    "Blues": [
+        "C",
+        "F",
+        "G"
+    ],
+
+    "Lo-fi": [
+        "Am",
+        "F",
+        "C",
+        "G"
+    ]
+}
