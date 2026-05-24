354b4e0fe7bc52c9e2a27468374a669b679b4117
adithyakini <67313225+adithyakini@users.noreply.github.com>
2026-05-24 10:59:26 +0530

    Update render.py

--- audio/render.py
+++ audio/render.py
@@ -1,15 +1,32 @@
 import subprocess
+import os
 
 
 def render_wav():
 
-    subprocess.run([
+    os.makedirs("generated", exist_ok=True)
+
+    command = [
         "fluidsynth",
         "-ni",
-        "soundfonts/FluidR3_GM.sf2",
+        "soundfonts/GeneralUser-GS.sf2",
         "generated/output.mid",
         "-F",
         "generated/output.wav",
         "-r",
         "44100"
-    ])
+    ]
+
+    result = subprocess.run(
+        command,
+        capture_output=True,
+        text=True
+    )
+
+    print(result.stdout)
+    print(result.stderr)
+
+    if result.returncode != 0:
+        raise Exception(
+            f"FluidSynth failed:\n{result.stderr}"
+        )
